4e8838a305531b9ba154e35325b6abfe3d6e179e
Kyle Tozer <kwtozer@gmail.com>
2016-12-26 02:21:30 -0500

    added comments

--- data.js
+++ data.js
@@ -1,5 +1,33 @@
 var outputDir = './data';
 
+// responsible for outputting the error log file
+function logError(error){
+  var outputStr = '';
+
+  var weekdays = [
+    'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'
+  ];
+
+  var months = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+  ];
+
+  for(var key in error){
+    outputStr += key + ': ' + error[key] + '\n';
+  }
+
+  outputStr = [weekdays[date.getDay()], months[date.getMonth()], date.getDate().toString(), date.getYear().toString(), date.toLocaleTimeString()].join(' ') + '\n' + outputStr;
+
+  fs.writeFileSync('./scraper-error.log', outputStr);
+  return console.log('There was an error, check the log for more details.');
+}
+
+function leadingZero(integer){
+  return integer.length === 1 ? '0' + integer : integer;
+}
+
 module.exports = {
-  outputDir: outputDir
+  outputDir: outputDir,
+  logError: logError,
+  leadingZero: leadingZero
 };
--- scraper.js
+++ scraper.js
@@ -1,51 +1,34 @@
 var fs = require('fs');
 var Xray = require('x-ray');
 var csv = require('json2csv');
-var outputDir = require('./data').outputDir;
+var data = require('./data');
 
 var x = Xray();
 var url = 'http://www.shirts4mike.com/';
 var shirts = new Promise(getShirtData);
 var date = new Date();
 
-
-function logError(error){
-  var outputStr = '';
-
-  var weekdays = [
-    'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'
-  ];
-
-  var months = [
-    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-  ];
-
-  for(var key in error){
-    outputStr += key + ': ' + error[key] + '\n';
-  }
-
-  outputStr = [weekdays[date.getDay()], months[date.getMonth()], date.getDate().toString(), date.getYear().toString(), date.toLocaleTimeString()].join(' ') + '\n' + outputStr;
-
-  fs.writeFileSync('./scraper-error.log', outputStr);
-  return console.log('There was an error, check the log for more details.');
-}
+var outputDir = data.outputDir;
 
 
 function getShirtData(resolve, reject){
   var shirtData = [];
 
+  // scrapes shirts.php for shirt urls using node module #1
   x(url + 'shirts.php', '.products', ['a@href'])(function(error, shirtUris){
-    if(error){ return logError(error) }
+    if(error){ return reject(error) }
 
+    // loops through array of shirt urls and scrapes each of those pages for the desired content
     shirtUris.forEach(function(shirtUri){
 
+      // creates an object containing all of the shirt data using the specified fields
       x(shirtUri, {
         'Title': '.section h1',
         'Price': '.price',
         'Image URL': '.section img@src'
 
       })(function(error, data){
-        if(error) { return logError(error) }
+        if(error) { return reject(error) }
 
         data.URL = shirtUri;
         data.Title = data.Title.replace(data.Price + ' ', '');
@@ -53,6 +36,7 @@ function getShirtData(resolve, reject){
 
         shirtData.push(data);
 
+        // once all of the shirt data has been collected, resolve the promise
         if(shirtData.length === shirtUris.length){
           return resolve(shirtData);
         }
@@ -66,20 +50,25 @@ if(!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir);
 }
 
+
 shirts.then(function(data){
-
-  function leadingZero(integer){
-    return integer.length === 1 ? '0' + integer : integer;
-  }
-
+  // properly formats file into csv using node module #2
   var outputData = csv({data: data, fields: Object.keys(data[0])});
+  var leadingZero = data.leadingZero;
 
   var day = date.getDate().toString();
   var month = (date.getMonth() + 1).toString();
   var year = date.getFullYear().toString();
 
+  // generates a string of the name formatting required for the csv file
   var outputFileName = outputDir + '/' + [leadingZero(day), leadingZero(month), year].join('-') + '.csv';
+
+  // removes any older files in the data folder before outputting the newest one
+  fs.readdirSync(outputDir).forEach(function(file){
+    fs.unlinkSync(outputDir + '/' + file);
+  });
 
   fs.writeFileSync(outputFileName, outputData);
   console.log('Shirt data created!');
-});
+
+}, data.logError); // error file output handling function
